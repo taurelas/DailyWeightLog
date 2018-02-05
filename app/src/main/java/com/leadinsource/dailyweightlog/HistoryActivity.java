cffc03bb7d343eca0819b91e443f52d84d962b35
Matt <mbeimcik@gmail.com>
2018-02-05 07:43:03 +0100

    MainActivity without Chart and not handling the "weight entered today" scenario"

--- app/src/main/java/com/leadinsource/dailyweightlog/HistoryActivity.java
+++ app/src/main/java/com/leadinsource/dailyweightlog/HistoryActivity.java
@@ -1,18 +1,12 @@
 package com.leadinsource.dailyweightlog;
 
-import android.content.ContentResolver;
-import android.content.Context;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-
-import com.leadinsource.dailyweightlog.db.DataContract;
 
 public class HistoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -39,7 +33,7 @@ public class HistoryActivity extends AppCompatActivity implements LoaderManager.
     }
 
     void setUpRecyclerView(Cursor cursor) {
-        HistoryAdapter adapter = new HistoryAdapter(cursor);
+        WeightAdapter adapter = new WeightAdapter(cursor);
 
         rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(this));
@@ -63,47 +57,4 @@ public class HistoryActivity extends AppCompatActivity implements LoaderManager.
         setUpRecyclerView(null);
     }
 
-    static class WeightLoader extends AsyncTaskLoader<Cursor> {
-
-        Cursor retCursor = null;
-        ContentResolver contentResolver;
-
-
-        WeightLoader(Context context) {
-            super(context);
-            contentResolver = context.getContentResolver();
-        }
-
-        @Override
-        protected void onStartLoading() {
-            Log.d("Loader", "onStartLoading");
-            if(retCursor!= null) {
-                deliverResult(retCursor);
-            } else {
-                forceLoad();
-            }
-        }
-
-        @Override
-        public Cursor loadInBackground() {
-            Log.d("Loader", "loadInBackground");
-            try {
-                return contentResolver.query(DataContract.WeightEntry.CONTENT_URI,
-                        null,
-                        null,
-                        null,
-                        DataContract.WeightEntry.COLUMN_DATE);
-            } catch(Exception e) {
-                Log.e("Loader", "Failed to asynchronously load data.");
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        public void deliverResult(Cursor data) {
-            retCursor = data;
-            super.deliverResult(data);
-        }
-    }
 }
